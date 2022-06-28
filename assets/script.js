e44117fef1f8697a523833fc8e4f9ec6d92dbbf9
jaysmith13 <janeasmith13@gmail.com>
2022-06-27 19:58:47 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
-// Assignment code here
+//Assignment code here
 const UppercaseCharCodes = arrayFromLowToHigh(65,90)
 const LowercaseCharCodes = arrayFromLowToHigh(97,122)
 const SpecialCharacterCharCode = arrayFromLowToHigh(33,47)
+const NumberCharCode=arrayFromLowToHigh(48,57)
 .concat(arrayFromLowToHigh(58,64))
 .concat(arrayFromLowToHigh(91,96))
 .concat(arrayFromLowToHigh(123,126))
-const NumberCharCode=arrayFromLowToHigh(48,57)
 
 
 //function array
@@ -17,20 +17,117 @@ function arrayFromLowToHigh(low,high){
   return array
 }
 
+function generatePassword(){
+  let selected = ""
+  let charCodes = null
 
+  function PasswordLengthOption(){
+    const PasswordLength = prompt('Enter password length (Mininmum characters 8 maxium characters 128');
+    if (PasswordLength <= 7){
+      window.alert('Try again, the number was '+passwordLength +', please enter a number between 8 and 128')
+      return PasswordLengthOption();
+    }if (PasswordLength >=129){
+      window.alert('Try again, the number was '+passwordLength +',please enter a number between 8 and 128');
+    return PasswordLengthOption();
+  }
+  else{
+    console.log(passwordLength)
+    return passwordLength;
+     }
+  }
+  //LowercaseCharcodes
+  function includeLowercaseOption(){
+    var includeLowercase = prompt('Would you like to use lowercase characters in your password? Type 1 for yes or 2 for No');
+  includeLowercase =parseInt (includeLowercase);
+    switch(includeLowercase){
+      case 1:
+        charCodes = LowercaseCharCodes;
+        selected = selected.concat('Lowercase Characters')
+        includeLowercaseOption();
+        break;
+        case 2:
+          includeLowercaseOption();
+          break;
+        default:
+          window.alert("Invalid option typed, please try again.");
+          includeLowercaseOption
+          break;
+    }
+  }
+//UppercaseCharCodes
+function includeUppercaseoption(){
+  var includeUppercaseoption = prompt('Would you like to use uppercase characters in your password? Type 1 for yes or 2 for No');
+includeUppercase =parseInt (includeLowercase);
+  switch(includeUppercaseoption){
+    case 1:
+      charCodes = UppercaseCharCodes;
+      selected = selected.concat('Uppercase Characters')
+      includeUppercaseOption();
+      break;
+      case 2:
+        includeUppercaseOption();
+        break;
+      default:
+        window.alert("Invalid option typed, please try again.");
+        includeUppercaseOption
+        break;
+  }
+}
+//SpecialCharactersCharCodes
+function SpecialCharacterOption(){
+  var includeSpecialCharacter = prompt('Would you like to use Special Character in your password? Type 1 for yes or 2 for No');
+  includeSpecialCharacter =parseInt (includeSpecialCharacter);
+  switch(includeSpecialCharacter){
+    case 1:
+        charCodes = charCodes.concat(SpecialCharacterCharCode);
+        selected = selected.concat(',SpecialCharacterOption')
+        includeNumbersOption();
+        break;
+    case 2:
+        includeNumbersOption();
+        break;
+      default:
+        window.alert("Invalid option typed, please try again.");
+        SpecialCharacterOption();
+        break;
+  }
+}
+//NumbersCharcodes
+function includeNumbersOption(){
+  var includeNumbersOption = prompt('Would you like to use Special Character in your password? Type 1 for yes or 2 for No');
+  includeNumbers =parseInt (includeNumbers);
+  switch(includeNumbers){
+    case 1:
+         charCodes = charCodes.concat(NumberCharCode);
+         selected = selected.concat(',and Numbers')
+         window.alert("Your created password will be" +passwordLength +"characters long. Consisting of" +selected);
+         break;
+      
+      case 2:
+        if(charCodes === null){
+        window.alert("Please select options for your password.");
+        return includeLowercaseOption();
+        }else;
+        window.alert("Your created password will be" +passwordLength +"characters long. Consisting of" +selected);
+         break;
+      default:
+        window.alert("Invalid option typed, please try again.");
+        includeNumbersOption();
+        break;
+  }
+}
 //password function 
 var PasswordLength = PasswordLengthOption();
 includeLowercaseOption()
 const passwordCharacters = []
-for (let i = 0; <PasswordLength;i++){
-  const CharacterCode = charCodes[Math.floor(Math.random *charCodes.length)]
-  passwordCharacters.push
-  (String.fromCharCode(CharacterCode))
+for (let i = 0; i < PasswordLength; i++) {
+  const CharacterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+  passwordCharacters.push(String.fromCharCode(CharacterCode))
 }
 return passwordCharacters.join('')}
 
 
-function generatePassword()
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
